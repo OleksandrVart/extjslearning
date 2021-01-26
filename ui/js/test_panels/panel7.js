c893b023a1cd711b0aaa7611723fb6d35aab0157
Alexandr Vart <programvart@gmail.com>
2021-01-26 20:25:56 +0200

    part 7

--- ui/js/test_panels/panel7.js
+++ ui/js/test_panels/panel7.js
@@ -32,7 +32,82 @@ panel7 = new Ext.Panel({
             xtype: 'panel',
             flex: 1,
             padding: 10,
-            html: 'Тут решение'
+            layout: 'border',
+            items: [
+                {
+                    xtype:'panel',
+                    region: 'center',
+                    style: {
+                        textAlign: 'center',
+                    },
+                    padding: 20,
+                    items: [
+                        {
+                            xtype: 'box',
+                            html: 'Тут большая центральная часть'
+                        }
+                    ]
+                },
+                {
+                    region: 'west',
+                    id: 'left-panel',
+                    title: 'Левая',
+                    split: true,
+                    width: 200,
+                    minSize: 100,
+                    maxSize: 300,
+                    collapsible: true,
+                    margins: '5 0 5 5',
+                    layout: {
+                        type: 'accordion',
+                        animate: true
+                    },
+                    items: [{
+                        title: 'Первая',
+                        html: '<p>Очень важная первая вкладка</p>',
+                        padding: 5,
+                        border: false,
+                    }, {
+                        title: 'Вторая',
+                        html: '<p>Так себе вторая</p>',
+                        padding: 5,
+                        border: false,
+                    }, {
+                        title: 'Третья',
+                        html: '<p>Ну и что-то там в третьей</p>',
+                        padding: 5,
+                        border: false,
+                    }]
+                },
+                {
+                    region: 'east',
+                    id: 'right-panel',
+                    title: 'Правая',
+                    split: true,
+                    width: 300,
+                    minSize: 100,
+                    maxSize: 500,
+                    collapsible: true,
+                    margins: '5 5 5 0',
+                    layout: 'column',
+                    items: [{
+                        cls: 'qwerty',
+                        flex: 1,
+                        padding: 5,
+                        title: 'One',
+                        columnWidth: 0.5,
+                        html: '<p>Столбец 1</p>',
+                        // border: false,
+                    }, {
+                        layout: 'fit',
+                        title: 'Two',
+                        padding: 5,
+                        columnWidth: 0.5,
+                        html: '<p>Столбец 2</p>',
+                        // border: false,
+                    }]
+                },
+            ]
         }
     ]
 });
