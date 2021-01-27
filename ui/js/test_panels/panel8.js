835af8169deeb9b1d465f1a0d1f6eac76f19dace
Alexandr Vart <programvart@gmail.com>
2021-01-27 09:49:08 +0200

    task8

--- ui/js/test_panels/panel8.js
+++ ui/js/test_panels/panel8.js
@@ -1,3 +1,67 @@
+var formPanel = new Ext.FormPanel({
+    labelWidth: 100,
+    title: 'Заполните форму',
+    frame:true,
+    bodyStyle:'padding:5px 5px 0',
+    width: 500,
+    defaults: {anchor: '100%'},
+    defaultType: 'textfield',
+    buttonAlign: 'center',
+    items: [
+        {
+            fieldLabel: 'ФИО',
+            name: 'name',
+            allowBlank:false
+        },{
+            fieldLabel: 'Пароль',
+            name: 'pass',
+            allowBlank:false
+        },{
+            fieldLabel: 'Email',
+            name: 'email',
+            vtype:'email',
+            allowBlank:false
+        }, {
+            xtype: 'textarea',
+            fieldLabel: 'Описание',
+            name: 'text',
+            allowBlank:false
+        }
+    ],
+
+    buttons: [{
+        text: 'Готово',
+        scope: this,
+        handler: function () {
+            var form = formPanel.getForm();
+            if(form.isValid()) {
+                var values = form.getValues();
+                var text = 'Имя - ' + values.name
+                    + '.</br>Пароль - ' + values.pass
+                    + '.</br>Почта - ' + values.email
+                    + '.</br>Дополнительная информация: ' + values.text;
+                result.removeAll();
+                result.add({
+                    xtype: 'box',
+                    html: text
+                });
+                result.doLayout();
+            }
+        }
+    }]
+});
+
+var result = new Ext.Panel({
+    xtype:'panel',
+    width: 500,
+    style: {
+        // textAlign: 'center',
+    },
+    padding: 20,
+    margin: 50,
+    items: []
+});
+
 panel8 = new Ext.Panel({
     title: 'Задание 8',
     listeners: {
@@ -31,7 +95,10 @@ panel8 = new Ext.Panel({
             xtype: 'panel',
             flex: 1,
             padding: 10,
-            html: 'Тут решение'
+            items: [
+                formPanel,
+                result
+            ]
         }
     ]
 });
